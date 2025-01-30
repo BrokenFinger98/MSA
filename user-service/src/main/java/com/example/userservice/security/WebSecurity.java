086bf322b8ea66d45a81b07867318ae709672b26
BrokenFinger98 <dbtjsdn980818@gmail.com>
2025-01-31 01:50:17 +0900

    docs: yml 수정

--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -1,8 +1,7 @@
 package com.example.userservice.security;
 
-import com.example.userservice.service.UserService;
 import java.util.function.Supplier;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -20,60 +19,65 @@ import org.springframework.security.web.access.intercept.RequestAuthorizationCon
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.IpAddressMatcher;
 
+import com.example.userservice.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class WebSecurity {
 
-    private final UserService userService;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-    private final Environment env;
+	private final UserService userService;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+	private final Environment env;
 
-    public static final String ALLOWED_IP_ADDRESS = "127.0.0.1";
-    public static final String SUBNET = "/32";
-    public static final IpAddressMatcher ALLOWED_IP_ADDRESS_MATCHER = new IpAddressMatcher(ALLOWED_IP_ADDRESS + SUBNET);
+	public static final String ALLOWED_IP_ADDRESS = "127.0.0.1";
+	public static final String SUBNET = "/32";
+	public static final IpAddressMatcher ALLOWED_IP_ADDRESS_MATCHER = new IpAddressMatcher(ALLOWED_IP_ADDRESS + SUBNET);
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // Configure AuthenticationManagerBuilder
-        AuthenticationManagerBuilder authenticationManagerBuilder =
-                http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		// Configure AuthenticationManagerBuilder
+		AuthenticationManagerBuilder authenticationManagerBuilder =
+			http.getSharedObject(AuthenticationManagerBuilder.class);
+		authenticationManagerBuilder.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
 
-        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+		AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
 
-        http.csrf(csrf -> csrf.disable());
+		http.csrf(csrf -> csrf.disable());
 
-        http.authorizeHttpRequests((authz) -> authz
-                                .requestMatchers(new AntPathRequestMatcher("/actuator/**")).permitAll()
-                                .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
-                                .requestMatchers(new AntPathRequestMatcher("/users", "POST")).permitAll()
-                                .requestMatchers(new AntPathRequestMatcher("/users", "GET")).permitAll()
-                                .requestMatchers(new AntPathRequestMatcher("/welcome")).permitAll()
-                                .requestMatchers(new AntPathRequestMatcher("/health_check")).permitAll()
-                                .requestMatchers(new AntPathRequestMatcher("/swagger-ui/**")).permitAll()
-                                .requestMatchers(new AntPathRequestMatcher("/swagger-resources/**")).permitAll()
-                                .requestMatchers(new AntPathRequestMatcher("/v3/api-docs/**")).permitAll()
-//                        .requestMatchers("/**").access(this::hasIpAddress)
-                                .requestMatchers("/**")
-                                .access(new WebExpressionAuthorizationManager(
-                                        "hasIpAddress('127.0.0.1') or hasIpAddress('211.227.146.72')"))// host pc ip address
-                                .anyRequest().authenticated()
-                )
-                .authenticationManager(authenticationManager)
-                .sessionManagement((session) -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		http.authorizeHttpRequests((authz) -> authz
+				.requestMatchers(new AntPathRequestMatcher("/actuator/**")).permitAll()
+				.requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
+				.requestMatchers(new AntPathRequestMatcher("/users", "POST")).permitAll()
+				.requestMatchers(new AntPathRequestMatcher("/users", "GET")).permitAll()
+				.requestMatchers(new AntPathRequestMatcher("/welcome")).permitAll()
+				.requestMatchers(new AntPathRequestMatcher("/health_check")).permitAll()
+				.requestMatchers(new AntPathRequestMatcher("/swagger-ui/**")).permitAll()
+				.requestMatchers(new AntPathRequestMatcher("/swagger-resources/**")).permitAll()
+				.requestMatchers(new AntPathRequestMatcher("/v3/api-docs/**")).permitAll()
+				//                        .requestMatchers("/**").access(this::hasIpAddress)
+				.requestMatchers("/**")
+				.access(new WebExpressionAuthorizationManager(
+					"hasIpAddress('127.0.0.1') or hasIpAddress('119.67.102.178')"))// host pc ip address
+				.anyRequest().authenticated()
+			)
+			.authenticationManager(authenticationManager)
+			.sessionManagement((session) -> session
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-        http.addFilter(getAuthenticationFilter(authenticationManager));
-        http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()));
-        return http.build();
-    }
+		http.addFilter(getAuthenticationFilter(authenticationManager));
+		http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()));
+		return http.build();
+	}
 
-    private AuthorizationDecision hasIpAddress(Supplier<Authentication> authentication, RequestAuthorizationContext object) {
-        return new AuthorizationDecision(ALLOWED_IP_ADDRESS_MATCHER.matches(object.getRequest()));
-    }
+	private AuthorizationDecision hasIpAddress(Supplier<Authentication> authentication,
+		RequestAuthorizationContext object) {
+		return new AuthorizationDecision(ALLOWED_IP_ADDRESS_MATCHER.matches(object.getRequest()));
+	}
 
-    private AuthenticationFilter getAuthenticationFilter(AuthenticationManager authenticationManager) throws Exception {
-        return new AuthenticationFilter(authenticationManager, userService, env);
-    }
+	private AuthenticationFilter getAuthenticationFilter(AuthenticationManager authenticationManager) throws Exception {
+		return new AuthenticationFilter(authenticationManager, userService, env);
+	}
 }
