bd2a2a029f70f073b84a4a00191c10493840e153
BrokenFinger98 <dbtjsdn980818@gmail.com>
2025-01-31 00:58:02 +0900

    feat: restTemplate를 활용한 api 호출

--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -1,82 +1,100 @@
 package com.example.userservice.service;
 
-import com.example.userservice.dto.UserDto;
-import com.example.userservice.entity.User;
-import com.example.userservice.repository.UserRepository;
-import com.example.userservice.vo.ResponseOrder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-import lombok.RequiredArgsConstructor;
+
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.entity.User;
+import com.example.userservice.repository.UserRepository;
+import com.example.userservice.vo.ResponseOrder;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
-    private final UserRepository userRepository;
-    private final BCryptPasswordEncoder passwordEncoder;
+	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder passwordEncoder;
+	private final Environment env;
+	private final RestTemplate restTemplate;
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(username);
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByEmail(username);
 
-        if (user == null)
-            throw new UsernameNotFoundException(username + ": not found");
+		if (user == null)
+			throw new UsernameNotFoundException(username + ": not found");
 
-        return new org.springframework.security.core.userdetails.User(
-                user.getEmail(),
-                user.getEncryptedPassword(),
-                true, true, true, true,
-                new ArrayList<>());
-    }
+		return new org.springframework.security.core.userdetails.User(
+			user.getEmail(),
+			user.getEncryptedPassword(),
+			true, true, true, true,
+			new ArrayList<>());
+	}
 
-    @Override
-    public UserDto createUser(UserDto userDto) {
-        userDto.setUserId(UUID.randomUUID().toString());
+	@Override
+	public UserDto createUser(UserDto userDto) {
+		userDto.setUserId(UUID.randomUUID().toString());
 
-        ModelMapper mapper = new ModelMapper();
-        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-        User user = mapper.map(userDto, User.class);
-        user.setEncryptedPassword(passwordEncoder.encode(userDto.getPassword()));
+		ModelMapper mapper = new ModelMapper();
+		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		User user = mapper.map(userDto, User.class);
+		user.setEncryptedPassword(passwordEncoder.encode(userDto.getPassword()));
 
-        userRepository.save(user);
+		userRepository.save(user);
 
-        return mapper.map(user, UserDto.class);
-    }
+		return mapper.map(user, UserDto.class);
+	}
 
-    @Override
-    public UserDto getUserById(String userId) {
-        User user = userRepository.findByUserId(userId);
-        if(user == null) {
-            throw new UsernameNotFoundException("user not found");
-        }
+	@Override
+	public UserDto getUserById(String userId) {
+		User user = userRepository.findByUserId(userId);
+		if (user == null) {
+			throw new UsernameNotFoundException("user not found");
+		}
 
-        UserDto userDto = new ModelMapper().map(user, UserDto.class);
-        List<ResponseOrder> orders = new ArrayList<>();
-        userDto.setOrders(orders);
-        return userDto;
-    }
+		UserDto userDto = new ModelMapper().map(user, UserDto.class);
 
-    @Override
-    public Iterable<User> getUsers() {
-        return userRepository.findAll();
-    }
+		/* Using as rest template */
+		String orderUrl = String.format(env.getProperty("order_service.url"), userId);
+		ResponseEntity<List<ResponseOrder>> orderListResponse =
+			restTemplate.exchange(
+				orderUrl, HttpMethod.GET, null, new ParameterizedTypeReference<List<ResponseOrder>>() {
+				});
 
-    @Override
-    public UserDto getUserDetailsByEmail(String email) {
-        User user = userRepository.findByEmail(email);
-        if (user == null)
-            throw new UsernameNotFoundException(email);
+		userDto.setOrders(orderListResponse.getBody());
 
-        ModelMapper mapper = new ModelMapper();
-        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		return userDto;
+	}
 
-        return mapper.map(user, UserDto.class);
-    }
+	@Override
+	public Iterable<User> getUsers() {
+		return userRepository.findAll();
+	}
+
+	@Override
+	public UserDto getUserDetailsByEmail(String email) {
+		User user = userRepository.findByEmail(email);
+		if (user == null)
+			throw new UsernameNotFoundException(email);
+
+		ModelMapper mapper = new ModelMapper();
+		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+		return mapper.map(user, UserDto.class);
+	}
 }
